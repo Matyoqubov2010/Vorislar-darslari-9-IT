abc0aab7a28d0771fe89a0aeeb1b18cabdb884fb
Matyoqubov2010 <umidbekm956@gmail.com>
2025-10-21 15:47:26 +0500

    7dars phyton list operations

---
+++ 7dars.py
@@ -0,0 +1,20 @@
+# """
+# 21.10.25
+# python list operations
+# """
+
+# ism = 'Bobur'
+# Uy_manzili='Toshkent sh,Chilonzor tumani'
+
+# print(ism.capitalize())
+# print(ism.upper())
+# print(Uy_manzili.capitalize())
+# print(Uy_manzili.title())
+
+# #phytonda list loyihalar bilan ishlash
+
+# hayvonlar=["mushuk","sigir"]
+
+# oila=['dada 26''ona25']
+
+
