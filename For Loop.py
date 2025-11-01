8c698666b0390980ff29ee2f75b0b5608a7e3798
Matyoqubov2010 <umidbekm956@gmail.com>
2025-11-01 16:17:57 +0500

    For loop

---
+++ For Loop.py
@@ -0,0 +1,18 @@
+
+
+# oquvchilar=["zuhriddin","zuhriddinbek","zuhriddinjon"]
+# print(f"assalomu aleykum hurmatli {oquvchilar},ishlar  qanday")        
+
+# for ism in oquvchilar:
+#     print(f"assalomu aleykum {ism.title()}")
+#     print(f"hayr hurmatli{ism.upper()}")
+
+# raqamlar=list((6,8,36,70,75))
+
+# for son in raqamlar:
+#     print(f"{son}sonini kvadrati: {son**2}")
+# for son in raqamlar:
+#     print(f"{son}sonni 9 ga bolganda{son/9}")
+raqamlar = range(27,54,7)
+for son in raqamlar:
+    print(f"{son}sonining 4 darajasi: {son**4}")
