8154838cf2e74c754fa247df00f3509321567842
Matyoqubov2010 <umidbekm956@gmail.com>
2025-11-04 16:24:35 +0500

    masalalar

---
+++ masalalar yechish.py
@@ -0,0 +1,18 @@
+# k = int(input("k sonini kiriting: "))
+# n = int(input("n sonini kiriting: "))
+
+# for i in range(n):
+#     print(k)
+
+# a = int(input("a sonini kiriting: "))
+# b = int(input("b sonini kiriting: "))
+
+# for i in range(a, b+1):
+#     print(i)
+
+# a = int(input("a sonini kiriting: "))
+# b = int(input("b sonini kiriting: "))
+
+# for i in range(b, a-1, -1):
+#     print(i)
+
