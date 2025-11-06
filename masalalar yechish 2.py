251e04c46ea1195d8d1560bf77c563875f1837c1
Matyoqubov2010 <umidbekm956@gmail.com>
2025-11-06 16:10:07 +0500

    masala 2

---
+++ masalalar yechish 2.py
@@ -0,0 +1,31 @@
+
+
+# k = int(input("k = "))
+# n = int(input("n = "))
+# for i in range(n):
+#     print(k)
+
+# a = int(input("a = "))
+# b = int(input("b = "))
+# for i in range(a, b + 1):
+#     print(i)
+
+# a = int(input("a = "))
+# b = int(input("b = "))
+# for i in range(b - 1, a, -1):
+#     print(i)
+# print("Chiqarilgan sonlar soni:", (b - a - 1))
+
+
+# narx = float(input("1 kg konfet narxi = "))
+# for i in range(1, 11):
+#     print(f"{i} kg konfet narxi = {narx * i}")
+
+
+# narx = float(input("1 kg konfet narxi = "))
+# for i in range(1, 11):
+#     print(f"{i/10:.1f} kg konfet narxi = {narx * i / 10}")
+
+narx = float(input("1 kg konfet narxi = "))
+for i in range(12, 21, 2):
+    print(f"{i/10:.1f} kg konfet narxi = {narx * i / 10}")
